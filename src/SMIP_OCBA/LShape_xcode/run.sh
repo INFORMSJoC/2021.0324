fc1082c798167c647756067e1b31183e05eb609a
Jiajun Xu <jjxu217@gmail.com>
2023-08-21 13:14:20 -0700

    update format

---
+++ src/SMIP_OCBA/LShape_xcode/run.sh
@@ -0,0 +1,12 @@
+./LShape_xcode 1d_F30_MinF1_20_FO10 ./spInput/ ./spOutput/model_d
+./LShape_xcode 1d_F45_MinF1_30_FO15 ./spInput/ ./spOutput/model_d
+./LShape_xcode 1d_F60_MinF1_40_FO20 ./spInput/ ./spOutput/model_d
+
+./LShape_xcode 2d_F30_MinF1_20_FO10 ./spInput/ ./spOutput/model_d
+./LShape_xcode 2d_F45_MinF1_30_FO15 ./spInput/ ./spOutput/model_d
+./LShape_xcode 2d_F60_MinF1_40_FO20 ./spInput/ ./spOutput/model_d
+
+./LShape_xcode 3d_F30_MinF1_20_FO10 ./spInput/ ./spOutput/model_d
+./LShape_xcode 3d_F45_MinF1_30_FO15 ./spInput/ ./spOutput/model_d
+./LShape_xcode 3d_F60_MinF1_40_FO20 ./spInput/ ./spOutput/model_d
+
